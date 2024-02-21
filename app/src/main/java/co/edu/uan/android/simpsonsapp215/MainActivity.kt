991b03553abfca68a9d4982cc26083d6e4e6bb3d
wjfatuan <wilson.forero@uan.edu.co>
2024-02-20 21:38:26 -0500

    Uso de listas y radio buttons

--- app/src/main/java/co/edu/uan/android/simpsonsapp215/MainActivity.kt
+++ app/src/main/java/co/edu/uan/android/simpsonsapp215/MainActivity.kt
@@ -2,6 +2,8 @@ package co.edu.uan.android.simpsonsapp215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import co.edu.uan.android.simpsonsapp215.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val simpsonNames = arrayListOf<String>("bart", "lisa")
+
+
         setContentView(binding.root)
         binding.btnBart.setOnClickListener {
             changeSimpson("bart")
@@ -22,6 +28,30 @@ class MainActivity : AppCompatActivity() {
         }
         binding.btnLisa.setOnClickListener {
             changeSimpson("lisa")
+        }
+        binding.rbBart.setOnClickListener {
+            changeSimpson("bart")
+        }
+        binding.rbHomer.setOnClickListener {
+            changeSimpson("homer")
+        }
+        binding.rbMarge.setOnClickListener {
+            changeSimpson("marge")
+        }
+        binding.rbLisa.setOnClickListener {
+            changeSimpson("lisa")
+        }
+
+        val myadapter = SimpsonsAdapter(this, R.layout.list_simpsons, simpsonNames)
+        binding.lvSimpsons.adapter = myadapter
+
+        binding.lvSimpsons.setOnItemClickListener { parent, view, position, id ->
+            println("parent: $parent, view: $view, position $position, id: $id")
+            changeSimpson(simpsonNames[position].lowercase())
+            simpsonNames.add("marge")
+            simpsonNames[0]="homer"
+            myadapter.notifyDataSetChanged()
+            println("array data $simpsonNames")
         }
     }
 
---
+++ app/src/main/java/co/edu/uan/android/simpsonsapp215/SimpsonsAdapter.kt
@@ -0,0 +1,18 @@
+package co.edu.uan.android.simpsonsapp215
+
+import android.app.Activity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class SimpsonsAdapter(val activity: Activity, val layout: Int, val data: ArrayList<String>)
+    : ArrayAdapter<String>(activity, layout, data) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = activity.layoutInflater.inflate(layout, null)
+        val tv = view.findViewById<TextView>(R.id.tvName)
+        tv.text = data[position]
+        return view
+    }
+}
